58d15012c18745944edb16affb99ded7f2155d88
Kostya Bychkov <constantine.bychkov@gmail.com>
2015-12-21 11:52:33 +0200

    add SBOM framework project

---
+++ SBOM/SBOM/SBOM.h
@@ -0,0 +1,9 @@
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SBOM.
+FOUNDATION_EXPORT double SBOMVersionNumber;
+
+//! Project version string for SBOM.
+FOUNDATION_EXPORT const unsigned char SBOMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBOM/PublicHeader.h>
